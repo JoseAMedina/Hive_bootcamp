8c5de8f21612bb1ce470fed0628a463e9c27e3bf
Jose A. Medina <130107405+JoseAMedina@users.noreply.github.com>
2023-09-26 16:22:41 +0200

    ex_C01

---
+++ ex_C01/C01_ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josmedin <josmedin@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 17:11:19 by josmedin          #+#    #+#             */
+/*   Updated: 2023/09/25 11:14:43 by josmedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	swap;
+	int	count;
+
+	count = 0;
+	while (count < (size - 1))
+	{
+		if (tab[count] > tab[count + 1])
+		{
+			swap = tab[count];
+			tab[count] = tab[count + 1];
+			tab[count + 1] = swap;
+			count = 0;
+		}
+		else
+			count++;
+	}
+}
